0b92c90d651a3d5ccf483f2527119b974f38845f
Thai Ha <halequocthai@outlook.com>
2024-01-09 16:45:29 +0700

    Add automatic migration for development environment

--- db/psql_conn.go
+++ db/psql_conn.go
@@ -1,9 +1,11 @@
 package db
 
 import (
+	"context"
 	"log"
 
 	_ "github.com/lib/pq"
+	"github.com/thaiha1607/foursquare_server/common/env"
 	"github.com/thaiha1607/foursquare_server/ent"
 )
 
@@ -23,5 +25,16 @@ func (p *PostgresDatabaseConnector) setupConnection() (db *ent.Client) {
 		log.Fatalf("failed opening connection to postgres: %v", err)
 	}
 	log.Println("connected to postgres")
+	// >>> AUTOMATIC MIGRATION START
+	// It should be used only for development purpose.
+	// USE IT WITH CAUTION!
+	if !env.IsProdEnv() {
+		ctx := context.Background()
+		if err := db.Schema.Create(ctx); err != nil {
+			log.Fatalf("failed creating schema resources: %v", err)
+		}
+		log.Println("automatic migration done")
+	}
+	// AUTOMATIC MIGRATION END <<<
 	return
 }
